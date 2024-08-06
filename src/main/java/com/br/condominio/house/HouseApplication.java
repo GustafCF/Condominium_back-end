aa25d76654cc197336f91cf6788e46e89ae404ff
Gustavo Cesar Franco <gustavo.franco@iti.gov.br>
2024-08-06 13:53:45 -0300

    Implementação dos pacotes

--- src/main/java/com/br/condominio/house/HouseApplication.java
+++ src/main/java/com/br/condominio/house/HouseApplication.java
@@ -2,7 +2,11 @@ package com.br.condominio.house;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.br.condominio.house.config.FileStorageConfig;
+
+@EnableConfigurationProperties({FileStorageConfig.class})
 @SpringBootApplication
 public class HouseApplication {
 
