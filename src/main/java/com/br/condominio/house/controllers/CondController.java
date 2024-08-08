21ed888ebb08fecc02d6c1d7fbabf0879ee2c348
Gustavo Cesar Franco <gustavo.franco@iti.gov.br>
2024-08-08 10:53:06 -0300

    File Download Controller

--- src/main/java/com/br/condominio/house/controllers/CondController.java
+++ src/main/java/com/br/condominio/house/controllers/CondController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,7 @@ public class CondController {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @PutMapping
     public ResponseEntity<CondModel> update(@PathVariable UUID id, @RequestBody CondModel entity){
         var obj = service.update(id, entity);
         return ResponseEntity.ok().body(obj);
